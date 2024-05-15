3d6c4c592d84470fed4541ce346ff58186f20343
Roberson Andrade <roberson_souza11@hotmail.com>
2024-05-14 21:38:33 -0300

    chore: configure firebase in project

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:city/app.dart';
 import 'package:city/repositories/irregularity_repository.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-// import 'package:flutter_config/flutter_config.dart';
+import 'package:flutter_config/flutter_config.dart';
 import 'package:timeago/timeago.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-  // await FlutterConfig.loadEnvVariables();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  await FlutterConfig.loadEnvVariables();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseFirestore.instance.settings =
+      const Settings(persistenceEnabled: true);
 
   setLocaleMessages('pt_BR', PtBrMessages());
 
